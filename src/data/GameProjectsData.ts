7310b19560740779bd75e71b12a4bf03a7b39d0b
Benjamin Crawford <bencatcraw@gmail.com>
2025-02-21 15:08:43 -1000

    final update hopefully

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -2,152 +2,131 @@ import ProjectData from '@/data/ProjectData.ts'
 
 export default [
     new ProjectData("project-1", "Life Before Death", "img/projects/lbd.png", 
-    `
+    `   
     <div class="paragraph">
-     <strong>Life Before Death</strong> is a thing of beauty that I am so proud of. It's available on Android because why not.
-     <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
+     <strong>Life Before Death</strong> is a walking simulator/escape room game focused on interactive storytelling, diverse puzzles, and high quality graphics.
     </div>
     <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
+    <strong> Trailer </strong>
     </div>
     <div class="paragraph center">
-        <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+        <iframe class="youtube" src="https://video.fastly.steamstatic.com/store_trailers/257071960/movie480_vp9.webm?t=1732215514" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div class="paragraph center">
+        <iframe src="https://store.steampowered.com/widget/3282650/" frameborder="0" width="646" height="190"></iframe>
     </div>
 
     <div class="paragraph">
-        Main features :
+        Utilizes :
         <ul>
-        <li>Some stu</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>Unreal Engine 5</li>
+        <li>C++</li>
+        <li>Blueprints</li>
+        <li>Quixel</li>
         </ul>
     </div>
 
     <div class="paragraph center">
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
+        <img class="pc-screenshot" src="img/projects/lbd2.png" alt="LBD Screenshot" />
+        <img class="pc-screenshot" src="img/projects/lbd1.png" alt="LBD Screenshot" />
     </div>
-    `, "#23bd69", true),
-    new ProjectData("project-2", "Singing Addict", "img/projects/project-2-icon.png", `
+    `, "#383838", true),
+    new ProjectData("project-2", "Cookie & Cream VR Cafe", "img/projects/ccc.png", `
     <div class="paragraph">
-        <strong>Optimistic Chubby Blog</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@adonyi-gabor-604571">Adonyi Gábor</a>.
+        <strong>Cookie & Cream</strong> is a coop VR cooking game where the players play as cats.
     </div>
     <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
+        <iframe width="800" height="450" src="https://www.youtube.com/embed/KfA0ykglSQY?si=XDOaRGSG-5YqXubV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
 
     <div class="paragraph">
-        Main features :
+        Utilizes :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>Unity</li>
+        <li>C#</li>
+        <li>VR</li>
+        <li>Online Multiplayer</li>
         </ul>
     </div>
 
     <div class="paragraph">
     <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+        Source code is available on <a href="https://github.com/bencatcraw/VRCATCAFE" target="_blank">GitHub</a>.
     </div>
     </div>
-
-    <div class="paragraph center">
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
     </div>
     `, "#5a78af"),
-    new ProjectData("project-3", "Drawing Overload", "img/projects/project-3-icon.png", `
+    new ProjectData("project-3", "homebound", "img/projects/header.jpg", `
     <div class="paragraph">
-        <strong>Drawing Overload</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@miphotography">Miesha Maiden</a>.
+        <strong>homebound</strong> is a astmospheric puzzle platformer about a lonely astronaut and fluffy new friend. homebound is available for free on Steam.
     </div>
     <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
+    <strong> Trailer </strong>
+    </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://video.fastly.steamstatic.com/store_trailers/256966958/movie480_vp9.webm?t=1693471926" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div class="paragraph center">
+        <iframe src="https://store.steampowered.com/widget/2568870/" frameborder="0" width="646" height="190"></iframe>
+    </div>
+    <div class="paragraph">
+        Utilizes :
+        <ul>
+        <li>Unity</li>
+        <li>C#</li>
+        </ul>
     </div>
 
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2568870/ss_5686460168acf81291a0a4951a6013f93c2bf29e.600x338.jpg?t=1695372893" alt="Drawing Overload Screenshot" />
+        <img class="pc-screenshot" src="https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2568870/ss_1e4dff82a0e0bb20989c0a6791b62fc8b80ef717.600x338.jpg?t=1695372893" alt="Drawing Overload Screenshot" />
+        <img class="pc-screenshot" src="https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2568870/ss_29a6848eb3f86abbaa06136231a7d110efc96879.600x338.jpg?t=1695372893" alt="Drawing Overload Screenshot" />
+        <img class="pc-screenshot" src="https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2568870/ss_07b0686833618613b394bcb2c9f612f1cc6c4301.600x338.jpg?t=1695372893" alt="Drawing Overload Screenshot" />
+    </div>
+    `, "#e48246"),
+    new ProjectData("project-4", "Mars Mech Mission", "img/projects/mmmlogo.png", `
+    <div class="paragraph">
+    <strong>Mars Mech Mission</strong> is a tower defense game about a lone dwarf who has to defend his space base from robotic crabs and scorpions.
+    </div>
+    <div class="paragraph center">
+        <img width="800" src="img/projects/mmm.png" alt="Eugeneable Screenshot" />
+    </div>
     <div class="paragraph">
         Main features :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>Unity</li>
+        <li>C#</li>
+        <li>Blender</li>
+        <li>FLStudio</li>
         </ul>
     </div>
 
     <div class="paragraph">
         <div class="notice">
-        Playable in the browser (WebGL) on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+        Playable in the browser on <a href="https://bencatcraw.itch.io/mmm/" target="_blank">itch.io</a>.
+        Source code available on <a href="https://github.com/bencatcraw/I3T4" target="_blank">GitHub</a>.
         </div>
     </div>
-
+    `, "#e48246"),
+    new ProjectData("project-5", "Untitled Mech Game", "img/projects/water_3.gif", `
+    <div class="paragraph">
+        <strong>Untitled Mech Game</strong> was created for the MechJam III game jam by the UH Manoa Game Dev Club. The game is about a mech that can switch between earth, water, and fire abilities that let it solve puzzles to progress through the levels.
+    </div>
     <div class="paragraph center">
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
+        <img width="800" src="img/projects/art363.png" alt="Eugeneable Screenshot" />
     </div>
-    `, "#383838"),
-    new ProjectData("project-4", "Eugeneable", "img/projects/project-4-icon.png", `
     <div class="paragraph">
-    <strong>Eugeneable</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-    <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@neo8iam">NEOSiAM 2020</a>.
-    </div>
-    
-    <div class="paragraph">
-        Main features :
+        Utilizes :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>Unity</li>
+        <li>C#</li>
         </ul>
     </div>
 
     <div class="paragraph">
         <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
+        Playable in the browser on <a href="https://bencatcraw.itch.io/untitled-mech-game" target="_blank">itch.io</a>.
         </div>
     </div>
-
-    <div class="paragraph center">
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Eugeneable Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Eugeneable Screenshot" />
-    </div>
-    `, "#e80fb7"),
-    new ProjectData("project-5", "Cloud Drew Land", "img/projects/project-5-icon.png", `
-    <div class="paragraph">
-        <strong>Cloud Drew Land</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@cottonbro">cottonbro</a>.
-    </div>
-    
-    <div class="paragraph">
-        Main features :
-        <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
-        </ul>
-    </div>
-
-    <div class="paragraph">
-        <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
-        </div>
-    </div>
-
-    <div class="paragraph center">
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
-    </div>`, "#e48246")
+`, "#5a78af")
 ];
--- src/data/OtherProjectsData.ts
+++ src/data/OtherProjectsData.ts
@@ -1,106 +1,89 @@
 import ProjectData from '@/data/ProjectData.ts'
 
 export default [
-    new ProjectData("project-6", "HAVEN ProjecTable", "img/projects/project-6-icon.png", `
+    new ProjectData("project-6", "HAVEN ProjecTable", "img/projects/havn1.jpg", `
     <div class="paragraph">
       <strong>HAVEN ProjecTable</strong> is a collaborative project between LAVA and HSEO focused on visualization of climate and energy data.
-      <br/>Image by <a href="https://www.pexels.com/fr-fr/@aldoalz" target="blank">Aldo Picaso</a>.
             </div>
             <div class="paragraph center">
-              <a href="https://www.microsoft.com/en-us/p/some-app" target="_blank"><img src="img/projects/ms-store-logo.png" alt="MS Store button" /></a>
-          </div>
+            <iframe width="800" height="450" src="https://www.youtube.com/embed/U9C2qxI-Weg?si=gz7IXD9TdPGFaTzI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div> 
 
-          <div class="paragraph">
-            Main features :
-            <ul>
-            <li>Some stuff</li>
-            <li>Some great stuff</li>
-            <li>More awesome stuff</li>
-            <li>And then some</li>
-            </ul>
-        </div>
-
-        <div class="paragraph center">
-          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" />
-          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" />
-          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" /> 
-        </div>
-    `, "#23bd69", false, true),
-    new ProjectData("project-7", "Happening @ Manoa", "img/projects/project-7-icon.png", `
-    <div class="paragraph">
-                <strong>Happening @ Manoa</strong> is an application for members of the UH Manoa community to see what events are going on in their community as well as search for events that aligns with their interests.
-                <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@karolina-grabowska">Karolina Grabowska</a>.
+        <div class="paragraph">
+              <div class="notice">
+                More information available <a href="https://energy.hawaii.gov/what-we-do/clean-energy-vision/" target="_blank">here</a>.
+              </div>
             </div>
 
+        <div class="paragraph center">
+        </div>
+    `, "#23bd69", false, true),
+    new ProjectData("project-7", "Happening @ Manoa", "img/projects/landing.png", `
+    <div class="paragraph">
+                <strong>Happening @ Manoa</strong> is an application for members of the UH Manoa community to see what events are going on in their community as well as search for events that aligns with their interests.
+            </div>
+            <div class="paragraph center">
+                <img width="800" src="img/projects/landingfinal.png" alt="HAM Screenshot" />
+            </div>
             <div class="paragraph">
-              Main features :
+              Utilizes :
               <ul>
-              <li>Some stuff</li>
-              <li>Some great stuff</li>
-              <li>More awesome stuff</li>
-              <li>And then some</li>
+              <li>Meteor</li>
+              <li>React</li>
+              <li>MongoDB</li>
               </ul>
           </div>
 
             <div class="paragraph">
               <div class="notice">
-                You can download and try the app for free or purchase it directly from <a href="http://www.somewbsite.com/" target="_blank">Some website</a>
+                You can find many more details on our  <a href="https://happeningatmanoa.github.io/" target="_blank">website</a>
               </div>
             </div>
-
-            <div class="paragraph center">
-                <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drew Blood Screenshot" />
-                <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drew Blood Screenshot" />
-                <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drew Blood Screenshot" /> 
-            </div>`, "#1ca1e2"),
-    new ProjectData("project-8", "Hawaii Healthcare Dashboard", "img/projects/project-8-icon.png", `
+`, "#1ca1e2"),
+    new ProjectData("project-8", "Hawaii Healthcare Dashboard", "img/projects/tylerLogo.png", `
     <div class="paragraph">
                 <strong>Hawaii Healthcare Dashboard</strong> A dashboard that allows users to search for healthcare facilities and filter them by name, services, insurance, island, and facility type. Created for Hawaii Annual Code Challenge 2023
             </div>
-
             <div class="paragraph center">
-              <a href="https://play.google.com/store/apps/details?id=someapp" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
-          </div>
-
+                <img width="800" src="img/projects/hhd.png" alt="HAM Screenshot" />
+            </div>
           <div class="paragraph">
-          Main features :
+          Utilizes :
           <ul>
-          <li>Some stuff</li>
-          <li>Some great stuff</li>
-          <li>More awesome stuff</li>
-          <li>And then some</li>
+          <li>Meteor</li>
+          <li>React</li>
+          <li>MongoDB</li>
           </ul>
       </div>
 
             <div class="paragraph">
               <div class="notice">
-                Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+                Source code available on <a href="https://github.com/bencatcraw/HACC2023" target="_blank">GitHub</a>.
               </div>
             </div>
 
             <div class="paragraph center">
-                <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="The Art of Walking Screenshot" />
-                <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="The Art of Walking Screenshot" />
             </div>`),
-    new ProjectData("project-9", "CreateX Horror Experience", "img/projects/project-9-icon.png", `
+    new ProjectData("project-9", "CreateX Horror Experience", "img/projects/crtx.png", `
      <div class="paragraph">
                 <strong>CreateX Horror Experience</strong> This is a horror experience created using the CreateX lab at UH West Oahu. CreateX is an array of projectors that project videos and applications onto the walls and floors of the room to create immersive experiences.
-                <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@goumbik">Lukas</a>.
             </div>
-
+            <div class="paragraph center">
+            <iframe width="800" height="450" src="https://www.youtube.com/embed/L0PQpKlKZmo?si=7W5L00i7KG2Sqq0e" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
             <div class="paragraph">
-        Main features :
+        Utilizes :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>Projection Mapping</li>
+        <li>Unity</li>
+        <li>C#</li>
+        <li>Motion Tracking</li>
         </ul>
     </div>
 
             <div class="paragraph">
               <div class="notice">
-                Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+                Source code available on <a href="https://github.com/bencatcraw/VRARCREATEX3" target="_blank">GitHub</a>.
               </div>
             </div>`),
 ];
